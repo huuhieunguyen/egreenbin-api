9a4a5e4e626b2e772a7da3fc56a0c8c586848fdf
huuhieunguyen <huuhieu.nguyen0408@gmail.com>
2023-03-07 10:35:53 +0700

    Add Code attr in Student api

--- models/student.go
+++ models/student.go
@@ -4,17 +4,11 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Student struct {
 	ID             primitive.ObjectID `bson:"_id" json:"id" `
+	Code           string             `bson:"code" json:"code"`
 	Name           string             `bson:"name" json:"name"`
-	Genre          int                `bson:"genre" json:"genre"`
 	NumOfCorrect   int                `bson:"NumOfCorrect" json:"NumOfCorrect"`
 	NumOfWrong     int                `bson:"NumOfWrong" json:"NumOfWrong"`
 	ImageAvatarUrl string             `bson:"ImageAvatarUrl" json:"ImageAvatarUrl"`
 	ParentEmail    string             `bson:"ParentEmail" json:"ParentEmail"`
 	Note           string             `bson:"Note" json:"Note"`
-	//int? numOfCorrect; // number of correct
-	// int? numOfWrong; // number of wrong
-	// String? imageAvatarUrl; // link url of image avatar student
-	//String? parentEmail; // email of parent's student
-	//String? note; // note when send email to parent's student
-	// defalut value is null or ""
 }
