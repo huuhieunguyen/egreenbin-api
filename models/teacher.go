76483eb903b99ead062798ef94e2cbdfd69a59e9
huuhieunguyen <huuhieu.nguyen0408@gmail.com>
2023-03-10 11:36:30 +0700

    Add Teacher api & lowercase Student attr

--- models/teacher.go
+++ models/teacher.go
@@ -2,13 +2,9 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-type Student struct {
+type Teacher struct {
 	ID             primitive.ObjectID `bson:"_id" json:"id" `
 	Code           string             `bson:"code" json:"code"`
 	Name           string             `bson:"name" json:"name"`
-	NumOfCorrect   int                `bson:"numOfCorrect" json:"numOfCorrect"`
-	NumOfWrong     int                `bson:"numOfWrong" json:"numOfWrong"`
 	ImageAvatarUrl string             `bson:"imageAvatarUrl" json:"imageAvatarUrl"`
-	ParentEmail    string             `bson:"parentEmail" json:"parentEmail"`
-	Note           string             `bson:"note" json:"note"`
 }
